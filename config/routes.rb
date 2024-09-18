8d1dba7799ea239551f41d5f12a19de9d228915d
Darian Thomas <darianthomascpt@gmail.com>
2024-09-18 10:48:16 +0200

    it was tough

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
 
   # Defines the root path route ("/")
   # root "posts#index"
